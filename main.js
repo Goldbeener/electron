537d2efa7eea41c6ffda06addc8f5657f1de3512
duxin <duxin@kuaishou.com>
2021-04-13 10:16:00 +0800

    feat(puzzle): 扩展主进程 通知

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
 const { app, BrowserWindow, Notification } = require('electron');
 const path = require('path');
-
+let win;
 function createWindow() {
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
         },
     });
 
     win.loadFile('index.html');
+    win.webContents.openDevTools();
 }
 
 function showNotification() {
-    const notification = {
+    const options = {
         title: '通知',
         body: '来自主进程的通知',
+        // actions: [{ type: 'button', text: '确定'}, { type: 'button', text: '知道了'}],
+        actions: [{ type: 'button', text: '知道了'}, { type: 'button', text: '确定'}],
+        closeButtonText: '关闭',
     };
-    new Notification(notification).show();
+    const notification = new Notification(options);
+
+    // 注意这些信息是主进程的，因此日志信息是打印在命令行内的
+    // 渲染进程的日志 才会输出在应用控制台
+    notification.on('click', (event, arg) => {
+        console.log('click>>>>', event, arg);
+    })
+
+    notification.on('close', (event, arg) => {
+        console.log('close>>>>', event, arg);
+    })
+
+    notification.on('action', (event, index) => {
+        console.log('action>>>>', event, index);
+    })
+    notification.show();
 }
 
 app.whenReady()
@@ -39,4 +59,5 @@ app.on('window-all-closed', () => {
     }
 });
 
+app
 app.addRecentDocument('./README.md');
